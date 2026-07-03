7d6082969d294d109fc84279711616114cf2db8b
keyssuegit <keyssue.com@gmail.com>
2026-07-03 17:43:31 +0900

    버튼 스타일 추가 및 수정

--- scratch/fetch_figma.js
+++ scratch/fetch_figma.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 const FIGMA_PERSONAL_ACCESS_TOKEN = 'figd_clXAeVN1_xkm42t83PQte9rOZg3LXL9hEBIjnFCH';
 const FIGMA_FILE_KEY = 'pO348u50Wve0jUSKBpmMyi';
-const FIGMA_NODE_ID = '2405:106456';
+const FIGMA_NODE_ID = '2405:106158';
 
 async function fetchFigmaNode() {
   const url = `https://api.figma.com/v1/files/${FIGMA_FILE_KEY}/nodes?ids=${FIGMA_NODE_ID}`;
